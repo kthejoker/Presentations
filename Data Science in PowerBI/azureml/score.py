ee4cddb0816f271a5ede2d9f9241e0e6b6ba0cee
Kyle Hale <kthejoker@gmail.com>
2020-05-13 14:31:15 -0500

    Titanic updates

---
+++ Data Science in PowerBI/azureml/score.py
@@ -0,0 +1,35 @@
+import pickle, json
+from azureml.core.model import Model
+import pandas as pd
+import numpy as np
+
+from inference_schema.schema_decorators import input_schema, output_schema
+from inference_schema.parameter_types.numpy_parameter_type import NumpyParameterType
+from inference_schema.parameter_types.pandas_parameter_type import PandasParameterType
+from sklearn.externals import joblib
+
+def init():
+    global titanic_classifier
+    model_path = Model.get_model_path(model_name = "titanic_classifier")
+    #model_path = "finalized_model.sav"
+    #with open(model_path, "rb") as f:
+    titanic_classifier = joblib.load(model_path)
+
+input_sample = pd.DataFrame(data=[{
+    "age": 20,
+    "pclass": 1,
+    "unaccompanied" : 0,
+    "sex" : 0
+}])
+
+output_sample = np.array([0])
+
+@input_schema('data', PandasParameterType(input_sample))
+@output_schema(NumpyParameterType(output_sample))
+def run(data):
+    try:
+        result = titanic_classifier.predict(data)
+        return result.tolist()
+    except Exception as e:
+        result = str(e)
+        return error
